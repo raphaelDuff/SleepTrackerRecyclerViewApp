11abe329a68f72374f31e2499a9a91ab6a2f6532
raphaelDuff <raphaelprates@outlook.com>
2021-03-06 23:56:58 -0300

    GridLayout Manager was added to the RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -63,6 +64,11 @@ class SleepTrackerFragment : Fragment() {
 
         binding.sleepList.adapter = adapter
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        //GridLayout Manager
+        val manager = GridLayoutManager(activity,3)
+        binding.sleepList.layoutManager = manager
+
         binding.lifecycleOwner = this
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
